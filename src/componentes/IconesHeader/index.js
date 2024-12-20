f1b8310d867779b371ead3d67ba55ae28bc4978c
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-20 15:22:28 -0300

    ++ Update

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,17 +1,27 @@
-import './IconesHeader.css'
 import perfil from '../../img/perfil.svg'
 import sacola from '../../img/sacola.svg'
+import style from 'styled-components'
+
+const Icone = style.li`
+    margin-right: 40px;
+    width: 25px;
+`
+
+const Icones = style.ul`
+    display: flex;
+    align-items: center;
+`
 
 const icones = [perfil, sacola]
 
 
 function IconesHeader() {
     return(
-        <ul className='icones'>
+        <Icones>
           { icones.map( (icone) => (
-            <li className='icone'><img src={icone} alt='icone'/></li>
+            <Icone><img src={icone} alt='icone'/></Icone>
           ))}
-        </ul>
+        </Icones>
     )
 
 }
