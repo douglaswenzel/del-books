489f80c8f4074c3d64b17a9d99ec268b5924378b
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-20 15:26:21 -0300

    ++ Update

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,17 +1,26 @@
-import './Logo.css'
 import logo from '../../img/logo.svg'
+import styled from 'styled-components'
 
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+    flex-direction: row;
+    padding: 25px;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
 
 function Logo() {
     return (
-    <div className="Logo">
-        <img 
+    <LogoContainer>
+        <LogoImage 
         src={logo}
-        className='logosvg'
-        alt='Logo DeltaBooks'
+        alt='Logo'
              />
         <p><strong>Delta</strong>Books</p>
-    </div>
+    </LogoContainer>
     )
 }
 
