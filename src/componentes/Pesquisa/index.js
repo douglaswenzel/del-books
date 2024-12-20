76865d02190ea119700b1199907e22a6f6a61f44
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-20 16:01:23 -0300

    Componente Input

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+
+function Pesquisa (){
+    return(
+        <section>
+            <h2>Já sabe por onde começar?</h2>
+            <h3>Encontre seu livro em nossa estante.</h3>
+            <input placeholder='Título desejado'></input>
+        </section>
+    )
+}
+
+export default Pesquisa
